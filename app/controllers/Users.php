5f7a6a5bfa96c43df656383dfc13584c10af2051
SiimIlves <siim.ilves.001@khk.ee>
2021-05-05 11:43:33 +0300

    Finish at home

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -15,7 +15,14 @@ class Users extends Controller
     public function register()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            print_r($_POST);
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = array(
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+            );
+            print_r($data);
         } else {
             $this->view('users/register');
         }
