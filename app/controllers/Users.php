9b935025f611b67f4d4b468b4880f0e59f4e7013
SiimIlves <siim.ilves.001@khk.ee>
2021-05-10 09:39:11 +0300

    Loodud parooli ja emaili väärtuste valideerimine

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -21,6 +21,32 @@ class Users extends Controller
                 'email' => trim($_POST['email']),
                 'password' => trim($_POST['password']),
                 'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password' => ''
+                if(emtpy($data['name'])){
+                    $data['name_err'] = "please enter the name";
+                }
+                if(emtpy($data['name'])){
+                    $data['email_err'] = "please enter the email";
+                } else if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+                    $data['email_err'] = "please enter a valid email";
+                } else if($this->usersModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'Email is already taken';
+                }
+                if(emtpy($data['name'])){
+                    $data['password_err'] = "please enter a password";
+                } else if(strlen($data['password'])<6) {
+                    $data['password_err'] = 'Password must consist of at least 6 characters.';
+                }
+                if(emtpy($data['name'])){
+                    $data['confirm_password_err'] = "please confirm the password";
+                } else if(strlen($data['confirm_password'])<6) {
+                    $data['confirm_password_err'] = 'Password must consist of at least 6 characters.';
+                } else if($data[password] !== $data['confirm_password']){
+                    $data['confirm_password_err'] = 'Passwords do not match';
+                }
             );
             print_r($data);
         } else {
--- app/models/User.php
+++ app/models/User.php
@@ -3,5 +3,19 @@
 
 class User
 {
-
+    private $db;
+    public function __construct()
+    {
+        $this->db = new database();
+    }
+    public function findUserByEmail($email){
+        $this->db->query('SELECT * FROM users WHERE email=:email');
+        $this->db->bind(':email', $email);
+        $user = $this->db->getOne();
+        if($this->db->rowCount()>8){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
